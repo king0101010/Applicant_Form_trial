0be68fd6ed32564ff39e48c325e0a3f59184c0fd
hitansh1926 <professionalplayers26@gmail.com>
2025-08-06 18:41:45 +0530

    Corrected path in SubmissionSuccess component for header and footer

--- src/components/common/SubmissionSuccess.jsx
+++ src/components/common/SubmissionSuccess.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from './header';
-import Footer from './footer';
+import Header from '../common/header';
+import Footer from '../common/footer';
 
 const SubmissionSuccess = () => {
   return (
